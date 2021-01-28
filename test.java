131a75585b1b3f6fb2eabbd73d48aa0a8e216a7a
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-01-29 00:39:53 +0600

    first upload

---
+++ test.java
@@ -0,0 +1,67 @@
+public class test
+{
+    // VARIABLE DECLARATION --- >  type variable_name = value;
+    // type variable_name;
+    
+
+    //FUNCTION DECLARATION
+    
+    /* access_modifier type functionName (ektaNUmberden,ektaSentenceDen)
+    {
+        //BODY
+        
+
+        //SET OF THINGS IT WILL DO
+
+
+    }*/
+
+    //--
+
+    static int sum(int firstValue,int secondValue)
+    {
+        int result;
+
+        result = firstValue+secondValue;
+
+        return result;
+
+    }
+
+
+
+
+
+    public static void main(String args[])
+    {
+        //DECLARATION PART
+        int a = 5;
+        int b = 7;
+
+        //---
+
+        //USAGE PART
+        a = a+b; 
+
+        //---
+
+        //FUNCTION USAGE /CALL
+        
+
+        // functionName(values for paramters separated by commas);
+
+        System.out.println(sum(7,9));
+
+
+        //------------
+
+
+    }
+}
+
+
+
+
+
+
+
